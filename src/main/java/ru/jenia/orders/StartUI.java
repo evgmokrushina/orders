3a13d024f61434b81eca016f3eced9261bc8526d
evgmokrushina <ewg.malysheva@yandex.ru>
2023-04-21 20:10:02 +0500

    Date. Date display.

---
+++ src/main/java/ru/jenia/orders/StartUI.java
@@ -0,0 +1,12 @@
+package ru.jenia.orders;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item one = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String created = one.getCreated().format(formatter);
+        System.out.println(created);
+    }
+}
