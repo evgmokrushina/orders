292932ce5cdc4afdfa3b4729fb32aafed856e2b5
evgmokrushina <ewg.malysheva@yandex.ru>
2023-05-05 19:04:51 +0500

    Implementation of the StartUI class. Menu display.

--- src/main/java/ru/jenia/orders/StartUI.java
+++ src/main/java/ru/jenia/orders/StartUI.java
@@ -1,14 +1,37 @@
 package ru.jenia.orders;
 
-import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class StartUI {
+    public void init(Scanner scanner, Orders orders) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.print("Select: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
+                run = false;
+            }
+        }
+    }
+
+    private void showMenu() {
+        String[] menu = {
+                "Add new Item", "Show all items", "Edit item",
+                "Delete item", "Find item by id", "Find items by name",
+                "Exit Program"
+        };
+        System.out.println("Menu:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+    }
+
     public static void main(String[] args) {
-        Item one = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String created = one.getCreated().format(formatter);
-        System.out.println(created);
-        Item two = new Item(1, "second");
-        System.out.println(two);
+        Scanner scanner = new Scanner(System.in);
+        Orders orders = new Orders();
+        new StartUI().init(scanner, orders);
     }
 }
