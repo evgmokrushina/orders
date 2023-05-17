6958c41d537755cb0e82082290aa317b56841f39
evgmokrushina <ewg.malysheva@yandex.ru>
2023-05-17 21:23:07 +0500

    Exception testing with junit.

---
+++ src/test/java/ru/jenia/ex/FactTest.java
@@ -0,0 +1,20 @@
+package ru.jenia.ex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class FactTest {
+
+    @Test
+    public void whenException() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    Fact.calc(-1);
+                }
+        );
+        assertThat(exception.getMessage()).isEqualTo("N could not be less than 0");
+    }
+}
