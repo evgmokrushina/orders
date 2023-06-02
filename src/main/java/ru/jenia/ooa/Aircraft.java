f65b24dbfa17adbb486df3ce64bb42750952335b
evgmokrushina <ewg.malysheva@yandex.ru>
2023-06-02 19:29:01 +0500

    Final

---
+++ src/main/java/ru/jenia/ooa/Aircraft.java
@@ -0,0 +1,7 @@
+package ru.jenia.ooa;
+
+public class Aircraft {
+    public void printModel() {
+        System.out.println("Метод выводит в консоль модель самолета.");
+    }
+}
---
+++ src/main/java/ru/jenia/ooa/Airport.java
@@ -0,0 +1,14 @@
+package ru.jenia.ooa;
+
+public class Airport {
+    public static void main(String[] args) {
+        final Airbus AIRBUS = new Airbus("A320");
+        System.out.println(AIRBUS);
+        AIRBUS.printModel();
+        AIRBUS.printCountEngine();
+        AIRBUS.setName("A380");
+        System.out.println(AIRBUS);
+        AIRBUS.printModel();
+        AIRBUS.printCountEngine();
+    }
+}
